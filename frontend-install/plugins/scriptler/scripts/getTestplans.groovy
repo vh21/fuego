9da6064378c05bb4f9c249c9f435148c000675ec
Dmitry Cherkassov <dcherkassov@gmail.com>
2015-11-18 01:44:22 +0300

    fix

--- frontend-install/plugins/scriptler/scripts/getTestplans.groovy
+++ frontend-install/plugins/scriptler/scripts/getTestplans.groovy
@@ -2,13 +2,13 @@ import jenkins.model.*
 import groovy.io.FileType
 
 def list = []
-last_used = new File("JTA_ENGINE_PATH_PLACEHOLDER/logs/"+test_name+"/last_used_testplan")
+last_used = new File("/home/jenkins/logs/"+test_name+"/last_used_testplan")
 if (last_used.exists()) {
   list += last_used.getText().replaceAll("\n","")
 }
   
   
-def dir = new File("JTA_ENGINE_PATH_PLACEHOLDER/overlays/testplans/")
+def dir = new File("/home/jenkins/overlays/testplans/")
 dir.eachFileRecurse (FileType.FILES) { file ->
   list += file.getName().split("\\.")[0]
 }
