19adf3b577eaec72b8d9ca5e629c5f840a61250e
Dmitry Cherkassov <dcherkassov@gmail.com>
2015-11-18 23:47:27 +0300

    remove scritpler from frontend install

--- frontend-install/plugins/scriptler/scripts/getTargets.groovy
+++
@@ -1,9 +0,0 @@
-import jenkins.model.*
-
-def result = []
-
-for(slave in Jenkins.instance.slaves) {
-  result += slave.name
-}
-
-return result
--- frontend-install/plugins/scriptler/scripts/getTests.groovy
+++
@@ -1,9 +0,0 @@
-import jenkins.model.*
-def result = []
-
-for (item in Jenkins.instance.items) {
-  if ( !(item.name =~ /^Run\ .*|^Service\..*|^Matrix\..*/) )
-    result += item.name
-}
-
-return result  
