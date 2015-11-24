bc8f7b4ef2f4241efbf3e089eb6bd3d354e1a851
Dmitry Cherkassov <dcherkassov@gmail.com>
2015-11-24 19:29:49 +0300

    update create-container

--- jta-host-scripts/docker-create-containter.sh
+++ jta-host-scripts/docker-create-containter.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo docker create -it -v /home/dc/wrk/cogent/jta-docker/userdata:/userdata jta
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+sudo docker create -it -v $DIR/../userdata:/userdata jta
