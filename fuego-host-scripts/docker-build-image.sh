9d8d1426c326575af8467f73bce7773d1f45a76c
Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
2017-01-13 10:26:49 +0900

    use host's gid, don't assume the user uid is the same as the gid
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>

--- fuego-host-scripts/docker-build-image.sh
+++ fuego-host-scripts/docker-build-image.sh
@@ -1,7 +1,9 @@
 if [ "$(id -u)" == "0" ]; then
 	JENKINS_UID=$(id -u $SUDO_USER)
+	JENKINS_GID=$(id -g $SUDO_USER)
 else
 	JENKINS_UID=$(id -u $USER)
+	JENKINS_GID=$(id -g $SUDO_USER)
 fi
 
-sudo docker build -t fuego --build-arg HTTP_PROXY=$http_proxy --build-arg uid=$JENKINS_UID .
+sudo docker build -t fuego --build-arg HTTP_PROXY=$http_proxy --build-arg uid=$JENKINS_UID --build-arg gid=$JENKINS_GID .
