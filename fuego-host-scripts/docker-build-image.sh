02f3d7370bb6512b251586100662be57bb11c7db
Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
2017-01-13 10:44:26 +0900

    fix copy paste issue with gid
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>

--- fuego-host-scripts/docker-build-image.sh
+++ fuego-host-scripts/docker-build-image.sh
@@ -3,7 +3,7 @@ if [ "$(id -u)" == "0" ]; then
 	JENKINS_GID=$(id -g $SUDO_USER)
 else
 	JENKINS_UID=$(id -u $USER)
-	JENKINS_GID=$(id -g $SUDO_USER)
+	JENKINS_GID=$(id -g $USER)
 fi
 
 sudo docker build -t fuego --build-arg HTTP_PROXY=$http_proxy --build-arg uid=$JENKINS_UID --build-arg gid=$JENKINS_GID .
