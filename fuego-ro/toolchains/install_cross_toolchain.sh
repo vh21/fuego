6dd9403c67c6ee41ba4f9b7b186cca9cb39e9cee
Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
2017-04-18 17:29:47 +0900

    crosstool: allow installing different architectures
    Before only armhf was available, now we can install
    arm64 armel armhf mips mipsel powerpc ppc64el toolchains
    
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>

--- fuego-ro/toolchains/install_cross_toolchain.sh
+++ fuego-ro/toolchains/install_cross_toolchain.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# $1: architecture (e.g.: arm64 armel armhf mips mipsel powerpc ppc64el)
+
+if [ -z ${1+x} ]; then
+    ARCH=armhf
+else
+    ARCH=$1
+fi
+
 echo deb http://emdebian.org/tools/debian/ jessie main > /etc/apt/sources.list.d/crosstools.list
-dpkg --add-architecture armhf
+dpkg --add-architecture $ARCH
 curl http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | sudo apt-key add -
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -yV install crossbuild-essential-armhf
+DEBIAN_FRONTEND=noninteractive apt-get -yV install crossbuild-essential-$ARCH
