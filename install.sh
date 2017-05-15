e4e6c6b130962b41df31c3066c70715b28d82b37
Tim Bird <tim.bird@am.sony.com>
2017-05-15 15:10:15 -0700

    Make sure ttc.conf goes into the 'conf' directory
    The 'conf' directory was missing, so install.sh was
    copying ttc.conf to the file 'fuego-ro/conf'.  Make
    a README placeholder file for the directory, and put
    ttc.conf into it explicitly.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 source fuego-host-scripts/docker-build-image.sh
-sudo /bin/sh -c "if [ -f /etc/ttc.conf -a ! -f fuego-ro/conf/ttc.conf ] ; then cp /etc/ttc.conf fuego-ro/conf ; fi"
+sudo /bin/sh -c "if [ -f /etc/ttc.conf -a ! -f fuego-ro/conf/ttc.conf ] ; then cp /etc/ttc.conf fuego-ro/conf/ttc.conf ; fi"
