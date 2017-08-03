f3899e414f56eadbf8980ab985b3d309a0dcffa6
Tim Bird <tim.bird@sony.com>
2017-08-03 15:56:41 -0700

    toolchains: fix issue with libaio-dev:$ARCH installation
    Change the way libaio-dev:$ARCH is installed.  If it is
    installed with 'apt-get install' it will remove another version
    of libaio-dev, which results in boards for that architecture having
    build failures.  In particular, if libaio-dev:armhf is installed,
    then LTP builds for docker will start failing with an
    error linking the test program io_submit01.  This is due
    to configure getting confused because /usr/include/libaio.h
    is present, but /usr/lib/<toolchain_dir>/libaio.so is missing
    (for x64_64, in this case).
    
    Instead of installing libaio-dev with apt-get, do a direct package
    download, extract the files, and only install a
    one file and symlink from /usr/lib/<toolchain_dir> into
    the docker container.
    
    See Fuego Issue 0048 for more details.
    
    Signed-off-by: Tim Bird <tim.bird@sony.com>

--- fuego-ro/toolchains/install_cross_toolchain.sh
+++ fuego-ro/toolchains/install_cross_toolchain.sh
@@ -17,6 +17,14 @@ case $ARCH in
 		;;
 esac
 
+# use working directory as a lock
+if [ -d /tmp/toolchain_install ] ; then
+    echo "Cannout use /tmp/toolchain_install as it's already present"
+    echo "Try again after other operation completes, or you remove it manually"
+    exit
+fi
+mkdir -p /tmp/toolchain_install
+
 echo deb http://emdebian.org/tools/debian/ jessie main > /etc/apt/sources.list.d/crosstools.list
 dpkg --add-architecture $ARCH
 curl http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | sudo apt-key add -
@@ -24,4 +32,23 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -yV install crossbuild-essential-$ARCH
 
 # libaio is needed for LTP
-DEBIAN_FRONTEND=noninteractive apt-get -yV install libaio-dev:$ARCH
+DEBIAN_FRONTEND=noninteractive apt-get -yV install libaio1:$ARCH
+
+# manually install libio-dev from the arch-specific libaio-dev debian package
+# (because 'apt-get install' removes any other libaio-dev package installed)
+echo "Install libaio-dev:$ARCH... (in a way that's a bit tricky)"
+SAVEDIR=$(pwd)
+cd /tmp/toolchain_install
+echo "  current directory: $(pwd)"
+apt-get download libaio-dev:$ARCH
+PKG=$(echo libaio-dev*.deb)
+echo "  downloaded package: $PKG"
+toolchain_dir=$(dpkg --contents $PKG | grep /usr/lib | head -n 2 | tail -n 1 | cut -b 59- )
+echo "  toolchain_dir: $toolchain_dir"
+# extract the package contents
+dpkg -x $PKG .
+# copy the file and symlink we need
+cp -vd usr/lib/${toolchain_dir}libaio.* /usr/lib/${toolchain_dir}
+# clean up
+cd $SAVEDIR
+rm -rf /tmp/toolchain_install
