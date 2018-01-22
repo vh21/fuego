eff2e2757e9b6de93c9489e7a34fd3564a7efd7a
Guilherme Campos Camargo <guicc@profusion.mobi>
2018-01-22 15:06:03 -0200

    Remove unused variables from docker build/create/run scripts
    The DIR variable is only being used by the CREATE scripts. There's no
    reason to keep it in the BUILD/REMOVE/START scripts as well.
    
    Signed-off-by: Guilherme Campos Camargo <guicc@profusion.mobi>

--- fuego-host-scripts/docker-remove-image.sh
+++ fuego-host-scripts/docker-remove-image.sh
@@ -1,12 +1,5 @@
+#!/bin/bash
 # $1 - name for the docker image (default: fuego)
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
 DOCKERIMAGE=${1:-fuego}
 
 sudo docker rmi ${DOCKERIMAGE}
