8e0d208fecbdf1597fbe9e4bd776f22da4cd4a01
Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
2018-05-09 17:38:54 +0900

    toolchains: add armel script for debian toolchains
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>

---
+++ fuego-ro/toolchains/debian-armel-tools.sh
@@ -0,0 +1,28 @@
+# fuego toolchain script
+# this sets up the environment needed for fuego to use a toolchain
+# this includes the following variables:
+# CC, CXX, CPP, CXXCPP, CONFIGURE_FLAGS, AS, LD, ARCH
+# CROSS_COMPILE, PREFIX, HOST, SDKROOT
+# CFLAGS and LDFLAGS are optional
+#
+# this script is sourced by ${FUEGO_RO}/toolchains/tools.sh
+#
+# Note that to use this script, you should install the
+# Debian cross-compiler toolchains, using the script
+# install_cross_toolchain.sh
+#
+# do this, inside the container, as follows:
+#  /fuego-ro/toolchain/install_cross_toolchain.sh armel
+
+export ARCH=arm
+
+export SDKROOT=/
+export PREFIX=arm-linux-gnueabi
+export_tools
+CPP="${CC} -E"
+CXXCPP="${CXX} -E"
+
+# save original path, to get to non-toolchain version of python
+ORIG_PATH=$PATH
+unset PYTHONHOME
+env -u PYTHONHOME
