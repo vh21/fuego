d27e5e4e6c1aa3d2fa030bd19a022fe1106d7024
Tim Bird <tim.bird@sony.com>
2019-01-11 16:25:20 -0800

    build: add NO_CACHE (in comment) for testing build scenarios
    You can use --no-cache to force "docker build" to not use the
    docker image cache.  This slows the build down, but is useful
    when trying to reproduce certain build failures.
    
    I *could* make this a command line argument, but that's too
    much complexity for how often I use this in debugging.
    For now, just add the option, so it's easy to turn on or off
    by uncommenting the line with the NO_CACHE shell variable.
    
    Signed-off-by: Tim Bird <tim.bird@sony.com>

--- fuego-host-scripts/docker-build-image.sh
+++ fuego-host-scripts/docker-build-image.sh
@@ -2,6 +2,10 @@
 # $1 - name for the docker image (default: fuego)
 DOCKERIMAGE=${1:-fuego}
 
+# uncomment this to avoid using the docker cache while building
+# (for testing)
+#NO_CACHE=--no-cache
+
 if [ "$(id -u)" == "0" ]; then
 	JENKINS_UID=$(id -u $SUDO_USER)
 	JENKINS_GID=$(id -g $SUDO_USER)
@@ -10,4 +14,4 @@ else
 	JENKINS_GID=$(id -g $USER)
 fi
 
-sudo docker build -t ${DOCKERIMAGE} --build-arg HTTP_PROXY=$http_proxy --build-arg uid=$JENKINS_UID --build-arg gid=$JENKINS_GID .
+sudo docker build ${NO_CACHE} -t ${DOCKERIMAGE} --build-arg HTTP_PROXY=$http_proxy --build-arg uid=$JENKINS_UID --build-arg gid=$JENKINS_GID .
