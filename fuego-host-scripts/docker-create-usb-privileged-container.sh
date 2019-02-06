731531578ca46b7b0fab17167af36d2b4856ec6e
Tim Bird <tim.bird@sony.com>
2019-02-06 15:24:37 -0800

    container: simplify finding script dir in container scripts
    Use 'realpath' to simplify the process of getting the canonical
    path to the directory containing the container scripts.
    
    Signed-off-by: Tim Bird <tim.bird@sony.com>

--- fuego-host-scripts/docker-create-usb-privileged-container.sh
+++ fuego-host-scripts/docker-create-usb-privileged-container.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # $1 - name for the docker image (default: fuego)
 # $2 - name for the docker container (default: fuego-container)
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+DIR=$(realpath "${BASH_SOURCE[0]}")
 
 DOCKERIMAGE=${1:-fuego}
 DOCKERCONTAINER=${2:-fuego-container}
